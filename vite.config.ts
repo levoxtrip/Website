5372da114a0a46d696e76a4941dbff33f90b68a6
levoxtrip <levoxtrip@gmail.com>
2025-05-11 11:32:42 +0200

    Changed Root

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/Website/',
-  plugins: [react(),
+  // Change this line to use root path for Vercel
+  base: '/',
+  plugins: [
+    react(),
     tailwindcss()
   ],
 })
